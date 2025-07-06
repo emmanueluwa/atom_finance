ddaeb26733179a2fcfa499646a3db2e885896e89
emmanuel <oemmanuel.uwadiae@gmail.com>
2025-07-07 00:01:01 +0100

    adding table and ratio list cards

---
+++ src/app/design-guide/page.tsx
@@ -0,0 +1,19 @@
+import RatioList from "@/components/RatioList/RatioList";
+import Table from "@/components/Table/Table";
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <h1>Atom Finance Design Guide</h1>
+      <RatioList />
+      <Table />
+      <h2>
+        This the design page. This is where all the design aspects of the app
+        can be found.
+      </h2>
+    </>
+  );
+};
+
+export default Page;
