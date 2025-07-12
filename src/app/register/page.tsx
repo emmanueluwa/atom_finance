92725319a19cb92f8a0f4536aa0c5e65b562b5e6
emmanuel <oemmanuel.uwadiae@gmail.com>
2025-07-12 18:36:42 +0100

    adding logout functionality to navbar

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -36,7 +36,7 @@ const Page = () => {
         <div className="w-full bg-white rounded-lg shadow dark:border md:mb-20 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign in to your account
+              Register
             </h1>
             <form
               className="space-y-4 md:space-y-6"
@@ -91,29 +91,12 @@ const Page = () => {
                 />
                 {errors.password ? <p>{errors.password.message}</p> : ""}
               </div>
-              <div className="flex items-center justify-between">
-                <a
-                  href="#"
-                  className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-                >
-                  Forgot password?
-                </a>
-              </div>
               <button
                 type="submit"
                 className="w-full text-white bg-yellow-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
                 Register
               </button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Don’t have an account yet?{" "}
-                <a
-                  href="#"
-                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                >
-                  Sign up
-                </a>
-              </p>
             </form>
           </div>
         </div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import React from "react";
 import logo from "@/../public/logo.jpg";
 import "./Navbar.css";
 import Image from "next/image";
 import Link from "next/link";
+import { useAuth } from "@/context/useAuth";
 
 const Navbar = () => {
+  const { isLoggedIn, user, logout } = useAuth();
+
   return (
     <nav className="relative container mx-auto p-6">
       <div className="flex items-center justify-between">
@@ -18,17 +23,31 @@ const Navbar = () => {
             </Link>
           </div>
         </div>
-        <div className="hidden lg:flex items-center space-x-6 text-back">
-          <Link href="/login" className="hover:text-yellow-600">
-            Login
-          </Link>
-          <Link
-            href="/register"
-            className="px-8 py-3 font-bold rounded text-white bg-black hover:opacity-70"
-          >
-            Signup
-          </Link>
-        </div>
+        {isLoggedIn() ? (
+          <div className="hidden lg:flex items-center space-x-6 text-back">
+            <div className="hover:text-yellow-600">
+              Welcome, {user?.username}
+            </div>
+            <button
+              onClick={logout}
+              className="px-8 py-3 font-bold rounded text-white bg-black hover:opacity-70"
+            >
+              Logout
+            </button>
+          </div>
+        ) : (
+          <div className="hidden lg:flex items-center space-x-6 text-back">
+            <Link href="/login" className="hover:text-yellow-600">
+              Login
+            </Link>
+            <Link
+              href="/register"
+              className="px-8 py-3 font-bold rounded text-white bg-black hover:opacity-70"
+            >
+              Register
+            </Link>
+          </div>
+        )}
       </div>
     </nav>
   );
